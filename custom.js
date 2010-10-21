0a2e254ccf64d7dd004988816e618a8ed31d4409
Hidden <zzdhidden@gmail.com>
2010-10-21 17:30:04 +0800

    First version

--- custom.js
+++ custom.js
@@ -1,35 +1,62 @@
-window.runtime && air.Introspector.Console.log( air.Localizer.localizer.getLocaleChain() );  
-var is_login = false;
-webim = window.webim;
-_path = "http://test.com/project/webim/ui/";
-	webim.ui.emot.init( {"dir": _path + "images/emot/default"} );
-var ui = new webim.ui(document.body, {
-	soundUrls: {
-		lib: _path + "assets/sound.swf",
-		msg: _path + "assets/sound/msg.mp3"
-	},
-	layoutOptions: {
-		loginOptions: {
-			questions: [["0", "安全提问(未设置请忽略)"], ["1", "母亲的名字"], ["2", "爷爷的名字"], ["3", "父亲出生的城市"], ["4", "你其中一位老师的名字"], ["5", "你个人计算机的型号"], ["6", "你最喜欢的餐馆名称"], ["7", "驾驶执照最后四位数字"]],
-			notice: "请使用UChome帐号登录"
+var _IMC = {};
+_IMC.path = "http://demo.webim20.cn/home/webim/";
+	_IMC.version = "";
+//_IMC.questions =  [["0", "安全提问(未设置请忽略)"], ["1", "母亲的名字"], ["2", "爷爷的名字"], ["3", "父亲出生的城市"], ["4", "你其中一位老师的名字"], ["5", "你个人计算机的型号"], ["6", "你最喜欢的餐馆名称"], ["7", "驾驶执照最后四位数字"]];
+_IMC.notice =  "请使用demo.webim20.cn/home帐号登录";
+(function(webim){
+	var path = _IMC.path;
+	var webim = window.webim;
+	webim.defaults.urls = {
+		online:path + "im.php?webim_action=online",
+		offline:path + "im.php?webim_action=offline",
+		message:path + "im.php?webim_action=message",
+		presence:path + "im.php?webim_action=presence",
+		refresh:path + "im.php?webim_action=refresh",
+		status:path + "im.php?webim_action=status"
+	};
+	webim.setting.defaults.url = path + "im.php?webim_action=setting";
+	webim.history.defaults.urls = {
+		load: path + "im.php?webim_action=history",
+		clear: path + "im.php?webim_action=clear_history",
+		download: path + "im.php?webim_action=download_history"
+	};
+	webim.room.defaults.urls = {
+		member: path + "im.php?webim_action=members",
+		join: path + "im.php?webim_action=join",
+		leave: path + "im.php?webim_action=leave"
+	};
+	webim.buddy.defaults.url = path + "im.php?webim_action=buddies";
+	//webim.notification.defaults.url = path + "im.php?webim_action=notifications";
+
+	webim.ui.emot.init({"dir": "app:/static/images/emot/default"});
+	var soundUrls = {
+		lib: "app:/static/assets/sound.swf",
+		msg: "app:/static/assets/sound/msg.mp3"
+	};
+	webim.ui.window.defaults.layoutUrl = "app:/air.window.html";
+	webim.ui.window.defaults.iconUrl = "app:/static/images/logo128.png";
+	var ui = new webim.ui(document.body, {
+		soundUrls: soundUrls,
+		layoutOptions: {
+			loginOptions: {
+				questions: _IMC.questions,
+				notice: _IMC.notice
+			}
+		},
+		imOptions: {
+			//jsonp: true
 		}
-	},
-	imOptions: {
-		//jsonp: true
-	}
-}), im = ui.im;
-//im.user({"uid":"1","id":"admin","nick":"admin","pic_url":"http:\/\/test.com\/project\/uc\/discuzX\/uc_server\/avatar.php?uid=0&size=small","url":"home.php?mod=space&uid=1"});
-//ui.addApp("menu", {"data": menu});
-//ui.layout.addShortcut( menu);
-ui.addApp( "buddy" );
-ui.addApp( "room" );
-//ui.addApp("notification");
-//ui.addApp("setting", {"data": webim.setting.defaults.data});
-ui.layout.showWidget( "buddy" );
-ui.render();
-is_login && im.autoOnline() && im.online();
-//im.online();
-im.bind("go", function(data){
-	data.connection.server = "http://test.com/project/webim/ui/im/test/" + data.connection.server;
-});
+	}), im = ui.im;
+	//im.user({"uid":"1","id":"admin","nick":"admin","pic_url":"http:\/\/test.com\/project\/uc\/discuzX\/uc_server\/avatar.php?uid=0&size=small","url":"home.php?mod=space&uid=1"});
+	//ui.addApp("menu", {"data": menu});
+	//ui.layout.addShortcut( menu);
+	ui.addApp( "buddy" );
+	ui.addApp( "room" );
+	//ui.addApp("notification");
+	//ui.addApp("setting", {"data": webim.setting.defaults.data});
+	ui.layout.showWidget( "buddy" );
+	ui.render();
+	//is_login && im.autoOnline() && im.online();
+	//im.online();
+})(webim);
 
--- updater.js
+++ updater.js
@@ -1,8 +1,8 @@
 //http://help.adobe.com/en_US/air/build/WS5b3ccc516d4fbf351e63e3d118666ade46-7ff2.html#WS96E10DFB-39A5-4488-A666-15B9B46C5EE8
 ( function() {
-	var updater = new air.update.ApplicationUpdaterUI();
+	var updater = new air.ApplicationUpdaterUI();
 	updater.configurationFile = new air.File("app:/updateConfig.xml");
-	updater.isCheckForUpdateVisible = true;
+	updater.isCheckForUpdateVisible = false;
 	updater.initialize();
 	// updater.currentVersion;
 	setTimeout( function() {
